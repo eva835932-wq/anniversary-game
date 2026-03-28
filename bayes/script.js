32bc2e8ff7732d6262b6b34d9bba563825a93d7b
eva835932-wq <eva835932@gmail.com>
2026-03-28 16:59:25 +0800

    Update script.js

--- bayes/script.js
+++ bayes/script.js
@@ -2,7 +2,7 @@ function checkAnswer() {
   const input = document.getElementById("answer").value;
 
   if (input === "5.4") {
-    window.location.href = "final.html";
+    window.location.href = "../climbing/index.html";
   } else {
     document.getElementById("result").innerText = "再想想 ✨";
   }
